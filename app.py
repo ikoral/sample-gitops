f4ed853c68026438575d45f4cfe795d5aa485908
ilhan koral <ilhankoral@outlook.com>
2023-03-12 00:24:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'ncaliber first pipeline setup. It is coool!!!'
